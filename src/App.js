95734d7c95533d771c08be3c291d832aaf73bf7d
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-04-23 14:12:15 +0200

    Adds Mui CSS framework for React as well as Mui Charts; adds a LineChart component with dummy data

--- src/App.js
+++ src/App.js
@@ -1,75 +1,114 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
-import DateInput from './DateInput';
-import ForecastMethodology from './ForecastMethodology';
-import InvestmentInput from './InvestmentInput';
-import NumberInput from './NumberInput';
-import DisplayFilter from './DisplayFilter';
-import ListButton from './ListButton';
-import StockList from './StockList';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import DateInput from "./DateInput";
+import ForecastMethodology from "./ForecastMethodology";
+import InvestmentInput from "./InvestmentInput";
+import NumberInput from "./NumberInput";
+import DisplayFilter from "./DisplayFilter";
+import ListButton from "./ListButton";
+import StockList from "./StockList";
+
+import LineChart from "./components/LineChart";
 
 function App() {
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [forecastMethodology, setForecastMethodology] = useState('polynomial_2'); // Set 'Quadratic Polynomial Regression' as the default Forecast Methodology
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [forecastMethodology, setForecastMethodology] =
+    useState("polynomial_2"); // Set 'Quadratic Polynomial Regression' as the default Forecast Methodology
   const [overallInvestmentBudget, setOverallInvestmentBudget] = useState(1000);
   const [stocks, setStocks] = useState([]);
   const [numberOfStocks, setNumberOfStocks] = useState(5);
   const [activeNumberOfStocks, setActiveNumberOfStocks] = useState(5);
-  const [displayFilter, setDisplayFilter] = useState('Show all');
-  
+  const [displayFilter, setDisplayFilter] = useState("Show all");
+
   const handleListStocks = async () => {
-    const allTickers = ['AAPL', 'AMZN', 'BAC', 'BRK-B', 'DIS', 'GOOGL', 'HD', 'JNJ', 'JPM', 'KO', 'MA', 'META', 'MSFT', 'NVDA', 'PG', 'TSLA', 'UNH', 'V', 'WMT', 'XOM'];
-  
+    const allTickers = [
+      "AAPL",
+      "AMZN",
+      "BAC",
+      "BRK-B",
+      "DIS",
+      "GOOGL",
+      "HD",
+      "JNJ",
+      "JPM",
+      "KO",
+      "MA",
+      "META",
+      "MSFT",
+      "NVDA",
+      "PG",
+      "TSLA",
+      "UNH",
+      "V",
+      "WMT",
+      "XOM",
+    ];
+
     try {
-        if (forecastMethodology === 'random') {
-          const selectedTickers = allTickers.sort(() => Math.random() - 0.5).slice(0, numberOfStocks);
-          const response = await axios.post('http://localhost:5000/get_stock_prices', {
-              startDate,
-              endDate,
-              tickers: selectedTickers
-          });
-          // Map to set forecast_price to null and then sort by ticker
-          const updatedStocks = response.data
-              .map(stock => ({
-                  ...stock,
-                  forecast_price: null
-              }))
-              .sort((a, b) => a.ticker.localeCompare(b.ticker));
-          
-          setStocks(updatedStocks); // Set the updated and sorted data to state
-      } else if (forecastMethodology === 'polynomial_2') {
-        // Fetch initial stock prices for all tickers to have base data
-        const baseResponse = await axios.post('http://localhost:5000/get_stock_prices', {
+      if (forecastMethodology === "random") {
+        const selectedTickers = allTickers
+          .sort(() => Math.random() - 0.5)
+          .slice(0, numberOfStocks);
+        const response = await axios.post(
+          "http://localhost:5000/get_stock_prices",
+          {
             startDate,
             endDate,
-            tickers: allTickers
-        });
+            tickers: selectedTickers,
+          }
+        );
+        // Map to set forecast_price to null and then sort by ticker
+        const updatedStocks = response.data
+          .map((stock) => ({
+            ...stock,
+            forecast_price: null,
+          }))
+          .sort((a, b) => a.ticker.localeCompare(b.ticker));
+
+        setStocks(updatedStocks); // Set the updated and sorted data to state
+      } else if (forecastMethodology === "polynomial_2") {
+        // Fetch initial stock prices for all tickers to have base data
+        const baseResponse = await axios.post(
+          "http://localhost:5000/get_stock_prices",
+          {
+            startDate,
+            endDate,
+            tickers: allTickers,
+          }
+        );
         // Fetch forecast data for all tickers
-        const forecastResponse = await axios.post('http://localhost:5000/forecast_stock_prices', {
+        const forecastResponse = await axios.post(
+          "http://localhost:5000/forecast_stock_prices",
+          {
             tickers: allTickers,
             start_date: startDate,
-            forecast_date: endDate
-        });
+            forecast_date: endDate,
+          }
+        );
         // Map forecast data back to base data and sort by growth
         const mappedAndSortedStocks = baseResponse.data
-          .map(stock => ({
-              ...stock,
-              forecast_price: forecastResponse.data.find(f => f.ticker === stock.ticker)?.forecast_price || null,
-              growth_percentage: forecastResponse.data.find(f => f.ticker === stock.ticker)?.growth_percentage || null
+          .map((stock) => ({
+            ...stock,
+            forecast_price:
+              forecastResponse.data.find((f) => f.ticker === stock.ticker)
+                ?.forecast_price || null,
+            growth_percentage:
+              forecastResponse.data.find((f) => f.ticker === stock.ticker)
+                ?.growth_percentage || null,
           }))
           .sort((a, b) => b.growth_percentage - a.growth_percentage)
           .slice(0, numberOfStocks);
-  
+
         setStocks(mappedAndSortedStocks); // Set sorted data with forecasts
       }
       setActiveNumberOfStocks(numberOfStocks);
     } catch (error) {
-      console.error('Error fetching stocks or forecast data:', error);
+      console.error("Error fetching stocks or forecast data:", error);
     }
   };
-  
+
   const handleNumberChange = (event) => {
     const number = parseInt(event.target.value, 10); // Convert input value to integer
     if (!isNaN(number) && number >= 2 && number <= 20) {
@@ -79,39 +118,49 @@ function App() {
 
   return (
     <div className="App">
+      <LineChart />
+
       {/* New container for the three-column layout */}
       <div className="three-column-container">
         {/* Area 1: Dates */}
         <div className="column">
-          <DateInput label="Start Date" name="startDate" onDateChange={setStartDate} />
-          <DateInput label="End Date" name="endDate" onDateChange={setEndDate} />
+          <DateInput
+            label="Start Date"
+            name="startDate"
+            onDateChange={setStartDate}
+          />
+          <DateInput
+            label="End Date"
+            name="endDate"
+            onDateChange={setEndDate}
+          />
         </div>
 
         {/* Area 2: Forecast Methodology and Display Filter */}
         <div className="column">
-          <ForecastMethodology 
-                  label="Forecast Methodology"
-                  onSelect={setForecastMethodology} 
-                  selectedMethodology={forecastMethodology}  // Pass the selected methodology as a prop
-              />
+          <ForecastMethodology
+            label="Forecast Methodology"
+            onSelect={setForecastMethodology}
+            selectedMethodology={forecastMethodology} // Pass the selected methodology as a prop
+          />
           <DisplayFilter onSelect={setDisplayFilter} />
         </div>
 
         {/* Area 3: Overall Investment Budget, Number of Stocks, and List Button */}
         <div className="column">
-          <InvestmentInput 
+          <InvestmentInput
             label="Overall Investment Budget"
             min={100} // Minimum of $100
             max={100000000000} // Maximum of $100 billion
             value={overallInvestmentBudget}
             onInvestmentChange={setOverallInvestmentBudget}
           />
-          <NumberInput 
-            label="Number of Stocks" 
-            name="numberOfStocks" 
-            value={numberOfStocks} 
-            onNumberChange={handleNumberChange} 
-            min={2} 
+          <NumberInput
+            label="Number of Stocks"
+            name="numberOfStocks"
+            value={numberOfStocks}
+            onNumberChange={handleNumberChange}
+            min={2}
             max={20}
           />
           <div className="column-button">
@@ -119,9 +168,9 @@ function App() {
           </div>
         </div>
       </div>
-      <StockList 
-        stocks={stocks} 
-        numberOfStocks={activeNumberOfStocks} 
+      <StockList
+        stocks={stocks}
+        numberOfStocks={activeNumberOfStocks}
         overallInvestmentBudget={parseFloat(overallInvestmentBudget)}
         displayFilter={displayFilter}
       />
