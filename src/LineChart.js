61d728dc6d08929065b72262978b2f858e49430e
Mladen Krivaćević <mladen.krivacevic@gmail.com>
2024-04-28 21:09:42 +0200

    Comments out the algorithm calculations, which will not be done here (in JavaScript) but on the server (in Python)

--- src/LineChart.js
+++ src/LineChart.js
@@ -5,23 +5,44 @@ import { format, addDays } from "date-fns";
 
 export default function SimpleLineChart(props) {
   // Extract Date from all the stock data from props
-  const xLabels = props.stock.map((u) => format(new Date(u.Date), "dd.MM."));
-  const priceData = props.stock.map(
-    (u) => Math.round((u.Close + Number.EPSILON) * 100) / 100
-  );
+  // const xLabels = props.stock.map((u) => format(new Date(u.Date), "dd.MM."));
 
+  // Return Dates for the previous 2 weeks
+  const xLabels = props.stock
+    .slice(-14)
+    .map((u) => format(new Date(u.Date), "dd.MM."));
+
+  // const priceData = props.stock.map(
+  //   (u) => Math.round((u.Close + Number.EPSILON) * 100) / 100
+  // );
+
+  // Return Close price for the last 2 weeks
+  const priceData = props.stock.slice(-14).map((u) => u.Close);
   let movingAverageData = [];
   let movingAverageDataPoint = 0;
 
-  for (let i = 0; i < priceData.length; i++) {
-    if (i > 0) {
-      movingAverageDataPoint = Math.abs(priceData[i] + priceData[i - 1]) / 2;
+  // Calculate moving average for the last 2 weeks
+  // for (let i = 0; i < props.stock.length; i++) {
+  //   if (i > 0) {
+  //     movingAverageDataPoint =
+  //       Math.abs(props.stock[i].Close + props.stock[i - 1].Close) / 2;
 
-      movingAverageData.push(movingAverageDataPoint);
-    } else {
-      movingAverageData.push(priceData[i]);
-    }
-  }
+  //     movingAverageData.push(movingAverageDataPoint);
+  //   } else {
+  //     movingAverageData.push(props.stock[i].Close);
+  //   }
+  // }
+  // console.log(movingAverageData);
+  // console.log(movingAverageData);
+
+  // for (let i = 0; i < priceData.length; i++) {
+  //   if (i > 0) {
+  //     movingAverageDataPoint = Math.abs(priceData[i] + priceData[i - 1]) / 2;
+
+  //     movingAverageData.push(movingAverageDataPoint);
+  //   } else {
+  //     movingAverageData.push(priceData[i]);
+  //   }
 
   return (
     <LineChart
@@ -34,11 +55,11 @@ export default function SimpleLineChart(props) {
           label: `${props.stock[0].Name} (Ticker: ${props.stock[0].Ticker})`,
           color: "blue",
         },
-        {
-          data: movingAverageData,
-          label: "Moving average",
-          color: "lightblue",
-        },
+        // {
+        //   data: movingAverageData,
+        //   label: "Moving average",
+        //   color: "lightblue",
+        // },
       ]}
       xAxis={[{ scaleType: "point", data: xLabels }]}
     />
